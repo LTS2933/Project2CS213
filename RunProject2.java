02f70dddb9168e8bb2d943e7bb8ef1cade45ea38
chrisosma24 <101012681+chrisosma24@users.noreply.github.com>
2023-02-16 10:57:05 -0500

    Create RunProject2.java

---
+++ RunProject2.java
@@ -0,0 +1,14 @@
+/**
+ * Driver class to run Project 2
+ * @author Christian Osma, Liam Smith
+ */
+public class RunProject2 {
+
+    /**
+     * Creates a new TuitionManager and calls the run method to start the project
+     * @param args arrray of Strings
+     */
+    public static void main (String [] args){
+        new TuitionManager().run();
+    }
+}
