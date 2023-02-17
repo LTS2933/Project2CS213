7e29fd52fcfb94a283ccc1532ad0ab75a846543d
chrisosma24 <101012681+chrisosma24@users.noreply.github.com>
2023-02-17 15:50:58 -0500

    Christian comments

--- TriState.java
+++ TriState.java
@@ -12,11 +12,14 @@ public class TriState extends NonResident {
 
     @Override
     public double tuitionDue(int creditsEnrolled) {
+        // Christian: good idea to use ? and : but == means that they have the same pointer instead of the same character sequence
+        // Christian: should be .equals("NY") instead
         return super.tuitionDue(creditsEnrolled) - (state == "NY" ? 4000 : 5000);
     }
 
     @Override
     public String toString(){
+        // Christian: should be this.state.toUpperCase()
         return super.toString() + " (tri-state:" + this.state + ")";
     }
 }
