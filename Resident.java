fbddc5a01225ddf77ff312e8ece68e2c66eb2277
chrisosma24 <101012681+chrisosma24@users.noreply.github.com>
2023-02-18 14:43:30 -0500

    Update Resident.java

--- Resident.java
+++ Resident.java
@@ -1,19 +1,34 @@
-public class Resident extends Student {
-    private int scholarship;
-    /*
+package src;
 
-     */
-
+public class Resident extends Student { 
     private static final int  tuitionResident = 15804;
     private static final int fullCredits = 16;
     private static final int minCredits = 12;
     private static final int partTimeResRate = 404;
     private static final double univFeePartTime = 2614.4;
-    public Resident(){
-        super();
-    }
+
+    private int scholarship;
+
     public Resident(int scholarship){
         super();
+        this.scholarship = scholarship;
+    }
+
+    public Resident(int scholarship, Profile profile, Major major, int creditsCompleted){
+        super(profile, major , creditsCompleted);
+        this.scholarship = scholarship;
+    }
+
+    public Resident(Profile profile, Major major, int creditsCompleted){
+        super(profile, major , creditsCompleted);
+        this.scholarship = 0;
+    }
+
+    public int getScholarship(){
+        return this.scholarship;
+    }
+
+    public void setScholarship(int scholarship){
         this.scholarship = scholarship;
     }
 
@@ -23,22 +38,14 @@ public class Resident extends Student {
         if (creditsEnrolled > fullCredits) return (partTimeResRate * (creditsEnrolled - fullCredits)) + tuitionResident - scholarship;
             return tuitionResident - scholarship;
     }
-    public double tuitionDue(int creditsEnrolled, int scholarship) {
-        if (creditsEnrolled > minCredits) return (partTimeResRate * (creditsEnrolled - minCredits)) + tuitionResident;
-        return tuitionResident;
-    }
 
     @Override
-    public boolean isResident() {
+    public boolean isResident(){
         return true;
     }
 
     @Override
     public String toString(){
-        return super.toString() + " (resident)";
+        return super.toString() + "(resident)";
     }
-    /*
-
-     */
-
 }
