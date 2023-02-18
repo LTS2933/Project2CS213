a4f5d00e096baf253da67445f0aa2a894ad1bcb5
chrisosma24 <101012681+chrisosma24@users.noreply.github.com>
2023-02-18 14:46:19 -0500

    Update TriState.java

--- TriState.java
+++ TriState.java
@@ -1,22 +1,28 @@
-public class TriState extends NonResident {
+package src;
+
+public class TriState extends NonResident { 
     private String state;
 
-    public TriState(){
-        super();
-        state = null;
+    public TriState(String state, Profile profile, Major major, int creditsCompleted){
+        super(profile, major, creditsCompleted);
+        this.state = state;
     }
-    public TriState(String state){
-        super();
+
+    public String getState(){
+        return this.state;
+    }
+
+    public void setState(String state){
         this.state = state;
     }
 
     @Override
-    public double tuitionDue(int creditsEnrolled) {
-        return super.tuitionDue(creditsEnrolled) - (state == "NY" ? 4000 : 5000);
+    public String toString(){
+        return super.toString() + "(tri-state:" + state.toUpperCase() + ")";
     }
 
     @Override
-    public String toString(){
-        return super.toString() + " (tri-state:" + this.state + ")";
+    public double tuitionDue(int creditsEnrolled) {
+        return super.tuitionDue(creditsEnrolled) - (state.equals("NY") ? 4000 : 5000);
     }
 }
